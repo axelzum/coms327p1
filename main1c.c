a8fec7cce9327cae0fcf26c28a72d0c977faeccf
Axel Zumwalt <axelzum@iastate.edu>
2020-11-01 15:29:49 -0600

    Change main to allow testing of process functions

--- main1c.c
+++ main1c.c
@@ -15,13 +15,38 @@
  */
 int main(int argc, char const *argv[]) {
 
+
+    sound *s = (sound*) malloc(sizeof(sound));
+    s->samples = malloc(16 * sizeof *s->samples);
+    s->samples[0] = 0.4;
+    s->samples[1] = 0.1;
+    s->samples[2] = 0.0;
+    s->samples[3] = 0.0;
+    s->samples[4] = 0.0;
+    s->samples[5] = 0.1;
+    s->samples[6] = 0.5;
+    s->samples[7] = 0.2;
+    s->samples[8] = 0.0;
+    s->samples[9] = 0.0;
+    s->samples[10] = 0.0;
+    s->samples[11] = 0.0;
+    s->samples[12] = 0.2;
+    s->samples[13] = 0.2;
+    s->samples[14] = 0.3;
+    s->samples[15] = 0.3;
+
+    s->length = 16;
+
+    s->rate = 60;
+
     FILE* f = fopen(argv[1], "w");
     if (f == NULL) {
         fprintf(stderr, "Invalid filename\n");
         return 1;
     }
-    sound* tone = genSawtooth(550.5, 7764, 1.0);
+    sound* tone = reverb(s, 0.05, 0.2);
     outputSound(tone, f);
+    free(tone->samples);
     free(tone);
     fclose(f);
 
--- makefile
+++ makefile
@@ -1,25 +1,7 @@
 partc: final
 
-all: parta partb
-
-partb: dtmf
-
-parta: gensine gendial
-
 final: main1c.c gensnd.c iosnd.c process.c
 	gcc -o main main1c.c gensnd.c iosnd.c process.c -lm
 
-dtmf: main1b.c gensndOLD.c
-	gcc -o dtmf main1b.c gensndOLD.c -lm
-
-gensine: main.c gensndOLD.c
-	gcc -o gensine main.c gensndOLD.c -lm
-
-gendial: main2.c gensndOLD.c
-	gcc -o gendial main2.c gensndOLD.c -lm
-
 clean:
-	rm -f gensine *~
-	rm -f gendial *~
-	rm -f dtmf *~
 	rm -f main *~
