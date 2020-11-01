92ff608973f610a68765abd4cda50d7ff6860ab1
Axel Zumwalt <axelzum@iastate.edu>
2020-11-01 15:30:45 -0600

    Part 4-7: Process functions: modulate, filter, reverb, echo

--- process.c
+++ process.c
@@ -2,6 +2,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#include <math.h>
+
 sound* mix(sound *s[], float w[], int c) {
     int longestSound = s[0]->length;
     int sampleRate = s[0]->rate;
@@ -10,7 +12,7 @@ sound* mix(sound *s[], float w[], int c) {
     for (i = 0 ; i < c; i++) {
         if (s[i]->rate != sampleRate) {
             fprintf(stderr, "Sounds in mix have differing sample rates.\n");
-            exit(1);
+            return NULL;
         }
         if (s[i]->length > longestSound) {
             longestSound = s[i]->length;
@@ -25,6 +27,7 @@ sound* mix(sound *s[], float w[], int c) {
 
     int j;
     for (i = 0; i < output->length; i++) {
+        output->samples[i] = 0.0;
         for (j = 0; j < c; j++) {
             if (s[j]->length > i) {
                 output->samples[i] += s[j]->samples[i] * w[j];
@@ -33,4 +36,97 @@ sound* mix(sound *s[], float w[], int c) {
     }
 
     return output;
+}
+
+sound* modulate(sound *s1, sound *s2) {
+    if (s1->rate != s2->rate || s1->length != s2->length) {
+        fprintf(stderr, "Sounds have differing sample rates or lengths.\n");
+        return NULL;
+    }
+
+    sound *output = (sound*) malloc(sizeof(sound));
+    output->samples = malloc(s1->length * sizeof *output->samples);
+    output->length = s1->length;
+    output->rate = s1->rate;
+
+    int i;
+    for (i = 0; i < output->length; i++) {
+        output->samples[i] = s1->samples[i]*s2->samples[i];
+    }
+
+    return output;
+}
+
+sound* filter(sound *s, float fir[], int c) {
+    sound *output = (sound*) malloc(sizeof(sound));
+    output->samples = malloc(s->length * sizeof *output->samples);
+    output->length = s->length;
+    output->rate = s->rate;
+
+    int i,j;
+    for (i = 0; i < output->length; i++) {
+        output->samples[i] = 0.0;
+        for (j = 0; j < c; j++) {
+            if (i>=j) {
+                output->samples[i] += s->samples[i-j] * fir[j];
+            }
+        }
+    }
+
+    return output;
+}
+
+sound* reverb(sound *s, float delay, float attenuation) {
+    if (s == NULL) {
+        fprintf(stderr, "Given sound is null.\n");
+        return NULL;
+    }
+    if (attenuation <= 0 || attenuation >= 1) {
+        fprintf(stderr, "Attenuation must be between 1 and 0\n");
+        return NULL;
+    }
+    if (delay <= 0 || delay >= 0.1) {
+        fprintf(stderr, "Delay must be bbetween 0.1 and 0.\n");
+        return NULL;
+    }
+
+    int firLength = round(delay * s->rate);
+    firLength++;
+    float fir[firLength];
+    int i;
+    for (i = 0; i< firLength; i++) {
+        fir[i] = 0.0;
+    }
+    fir[0] = 1.0;
+    fir[firLength-1] = attenuation;
+
+    return filter(s, fir, firLength);
+
+}
+
+sound* echo(sound *s, float delay, float attenuation) {
+    if (s == NULL) {
+        fprintf(stderr, "Given sound is null.\n");
+        return NULL;
+    }
+    if (attenuation <= 0 || attenuation >= 1) {
+        fprintf(stderr, "Attenuation must be between 1 and 0\n");
+        return NULL;
+    }
+    if (delay <= 0.1 || delay >= 1.0) {
+        fprintf(stderr, "Delay must be bbetween 0.1 and 1.\n");
+        return NULL;
+    }
+
+    int firLength = round(delay * s->rate);
+    firLength++;
+    float fir[firLength];
+    int i;
+    for (i = 0; i< firLength; i++) {
+        fir[i] = 0.0;
+    }
+    fir[0] = 1.0;
+    fir[firLength-1] = attenuation;
+
+    return filter(s, fir, firLength);
 }
--- process.h
+++ process.h
@@ -3,4 +3,12 @@
 
 sound* mix(sound *s[], float w[], int c);
 
+sound* modulate(sound *s1, sound *s2);
+
+sound* filter(sound *s, float fir[], int c);
+
+sound* reverb(sound *s, float delay, float attenuation);
+
+sound* echo(sound *s, float delay, float attenuation);
+
 #endif
